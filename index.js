155374193cf72b72faa34601624942846bf8c49a
Vitaliy <v1taliyanisimov@yandex.ru>
2022-09-08 08:03:37 +0300

    fix port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import updateInterval from "./fetchInterval.js";
 import { kazan } from "./fetchToUrl.js";
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
