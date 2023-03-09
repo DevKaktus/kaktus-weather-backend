941e70f21431896ea9258f38b004e3cf4807feae
Vitaliy <v1taliyanisimov@yandex.ru>
2023-03-09 14:46:10 +0300

    fix

--- fetchInterval.js
+++ fetchInterval.js
@@ -2,12 +2,12 @@ import { fetchToUrl } from "./fetchToUrl.js";
 
 export default function updateInterval() {
   fetchToUrl(
-    "http://api.openweathermap.org/data/2.5/forecast?q=Kazan&units=metric&appid=302f3545e76fc5e9d7af285032706f41"
+    "http://api.openweathermap.org/data/2.5/forecast?q=Kazan&units=metric&appid=302f3545e76fc5e9d7af285032706f41&lang=ru&units=metric"
   );
   setInterval(
     () =>
       fetchToUrl(
-        "http://api.openweathermap.org/data/2.5/forecast?q=Kazan&units=metric&appid=302f3545e76fc5e9d7af285032706f41"
+        "http://api.openweathermap.org/data/2.5/forecast?q=Kazan&units=metric&appid=302f3545e76fc5e9d7af285032706f41&lang=ru&units=metric"
       ),
     3600000
   );
